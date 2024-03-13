2ee905a2a2e0679a4e88fa03376a850669f968c8
Noiker-UX <pasha_pasha_201@mail.ru>
2024-03-13 12:11:44 +0200

    detail dlc.1

--- app/(site)/posts/[alias]/page.tsx
+++ app/(site)/posts/[alias]/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next';
 import style from './page.module.css';
 import { notFound } from 'next/navigation';
 import { Like, Tag } from '@/components';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Деталка',
@@ -23,6 +24,8 @@ export default async function detail({ params }: { params: { alias: string } }) 
         <Tag tag='light'>3 минут(ы)</Tag>•
         <Like countLikes={5} showCounter />
       </div>
+      <Image alt='Детальная картинка' src={'/Grid.png'} width={500} height={400}></Image>
+      <p>{detail.body}</p>
     </div>
   );
 }
